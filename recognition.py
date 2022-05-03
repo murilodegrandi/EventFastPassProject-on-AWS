18ce2e7685321a9fd7e893b9c85eee5b32b44fc3
Debian <bitnami@ip-172-26-12-133.ec2.internal>
2022-05-03 02:21:13 +0000

    new code

--- recognition.py
+++ recognition.py
@@ -1,16 +1,15 @@
 import boto3
-import json
-import urllib.request
-import urllib.parse
-import urllib.error
+
 if __name__ == "__main__":
 
-    bucket = 'lightsailphotos'
+    region = "us-east-1"
+    bucket = 'rekogpicturess3'
     collectionId = 'EFP_Collection-1'
-    fileName = urllib.parse.unquote_plus(['Records'][0]['s3']['object']['key'])
-    threshold = 98
+    fileName = 'sergio.jpg'
+    threshold = 70
     maxFaces = 2
-    client = boto3.client('rekognition')
+
+    client = boto3.client('rekognition', region_name=region)
 
     response = client.search_faces_by_image(CollectionId=collectionId,
                                             Image={'S3Object': {'Bucket': bucket, 'Name': fileName}},
@@ -22,4 +21,5 @@ if __name__ == "__main__":
     for match in faceMatches:
         print('FaceId:' + match['Face']['FaceId'])
         print('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
-        print
+        print(response)
+        print(faceMatches)
