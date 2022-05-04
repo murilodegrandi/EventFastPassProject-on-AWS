5fc4423602fd82d961786e39a7fa8d641bc7f7e1
Debian <bitnami@ip-172-26-12-133.ec2.internal>
2022-05-04 03:58:43 +0000

    files deleted

--- EventFastPassProject/rekognitionV2.py
+++ EventFastPassProject/rekognitionV2.py
@@ -12,6 +12,8 @@ KEY = 'check.jpg' # replace with your object key
 
 s3 = boto3.resource('s3')
 
+
+
 try:
     s3.Bucket(BUCKET_NAME).download_file(KEY, 'check.jpg')
 except botocore.exceptions.ClientError as e:
@@ -20,30 +22,46 @@ except botocore.exceptions.ClientError as e:
     else:
         raise
 
-with open(photo, 'rb') as image:
-    response = client.search_faces_by_image(CollectionId=collectionId,
-    Image={'Bytes': image.read()},
-    FaceMatchThreshold=threshold, MaxFaces=maxFaces)
+try:
+    with open(photo, 'rb') as image:
+        response = client.search_faces_by_image(CollectionId=collectionId,
+        Image={'Bytes': image.read()},
+        FaceMatchThreshold=threshold, MaxFaces=maxFaces)
+        
+    faceMatches = response['FaceMatches']
+    print(faceMatches)
 
-faceMatches = response['FaceMatches']
-print(faceMatches)
-
-for match in faceMatches:
-    print('FaceId:' + match['Face']['FaceId'])
-    print('ImageId:' + match['Face']['ImageId'])
-    print('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
-    print('Confidence: ' + str(match['Face']['Confidence']))
+    for match in faceMatches:
+        print('FaceId:' + match['Face']['FaceId'])
+        print('ImageId:' + match['Face']['ImageId'])
+        print('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
+        print('Confidence: ' + str(match['Face']['Confidence']))
     
-#Send SNS Topic 
+    #Send SNS Topic 
 
-# Create an SNS client
-client = boto3.client('sns', region_name=region)
-sns_resource = boto3.resource("sns", region_name=region)
-# Create the topic if it doesn't exist (this is idempotent)
-#topic = client.create_topic(Name="notifications")
-#topic_arn = topic['TopicArn']  # get its Amazon Resource Name
-# Publish a message.
-client.publish(Message="Facial Recognition Successful!\n\nSimilarity: " + 
-"{:.2f}".format(match['Similarity']) + "% \n" + 'Confidence: ' + str(match['Face']['Confidence']) + 
-'\n Please click here to scan ticket: https://eventfastpass.link/scan', 
-TopicArn="arn:aws:sns:us-east-1:656214810243:GuestCheck-InAlert")
+    # Create an SNS client
+    client = boto3.client('sns', region_name=region)
+    sns_resource = boto3.resource("sns", region_name=region)
+        
+    # Publish a message.
+    client.publish(Message="Facial Recognition Successful!\n\nSimilarity: " + 
+    "{:.2f}".format(match['Similarity']) + "% \n" + 'Confidence: ' + str(match['Face']['Confidence']) + 
+    '\n Please click here to scan ticket: https://eventfastpass.link/scan', 
+    TopicArn="arn:aws:sns:us-east-1:656214810243:GuestCheck-InAlert")
+
+except NameError as e:
+    print('Facial Recognition completed.\nResult = Not found')
+     #Send SNS Topic 
+
+    # Create an SNS client
+    client = boto3.client('sns', region_name=region)
+    sns_resource = boto3.resource("sns", region_name=region)
+        
+    # Publish a message.
+    client.publish(Message="<<<Identity not found!>>>\n\n This person does not have authorization to attend the event!", 
+    TopicArn="arn:aws:sns:us-east-1:656214810243:GuestCheck-InAlert")
+
+
+    
+
+
